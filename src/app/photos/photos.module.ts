f6b7a011c4897a8a72417d14f1bd3966d8c5701b
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-04-13 19:55:01 -0700

    organized the project into sub-modules

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,25 +1,12 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {CommonModule} from '@angular/common';
 
-import {PhotoComponent} from './photo/photo.component';
-import {PhotosComponent} from './photo-list/photos/photos.component';
-import {PhotoListComponent} from './photo-list/photo-list.component';
-import {FilterByDescriptionPipe} from './photo-list/filterByDescription.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import {PhotoModule} from './photo/photo.module';
+import {PhotoListModule} from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotosComponent,
-        PhotoListComponent,
-        PhotosComponent,
-        FilterByDescriptionPipe,
-        LoadButtonComponent
-    ],
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule {
