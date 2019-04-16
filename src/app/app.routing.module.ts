e2f8b17adff23ac2449908b89d351afb408fbb5a
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-04-16 09:34:51 -0700

    added the login page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,8 +4,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {PhotoListComponent} from './photos/photo-list/photo-list.component';
 import {NotFoundComponent} from './errors/not-found/not-found.component';
 import {PhotoListResolver} from './photos/photo-list/photo-list.resolver';
+import {SignInComponent} from './home/sign-in/sign-in.component';
 
 const ROUTES: Routes = [
+    {
+        path: '',
+        component: SignInComponent
+    },
     {
         path: 'user/:userName',
         component: PhotoListComponent,
---
+++ src/app/home/sign-in/sign-in.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './sign-in.component.html'
+})
+export class SignInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
